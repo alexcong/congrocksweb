9c33073ddd9268b8812addb052083185749f437a
alexcong <alexcong@gmail.com>
2016-05-30 15:44:09 -0400

    update middlewares

--- server/app.js
+++ server/app.js
@@ -1,13 +1,26 @@
 /**
  * Created by alexcong on 5/30/2016.
  */
-var koa = require('koa');
+var app = require('koa')();
+var middlewares = require('koa-middlewares');
 var path = require('path');
-var serve = require('koa-static');
 
-var app = koa();
+app
+    .use(middlewares.staticCache(path.resolve(__dirname, '../client/public')))
+    .use(middlewares.staticCache(path.resolve(__dirname, '../bower_components')))
+    .use(middlewares.bodyParser())
+    .use(middlewares.favicon())
+    .use(middlewares.logger())
+;
+var router = middlewares.router();
 
-app.use(serve(path.resolve(__dirname, '../client/public')));
+/* router */
+
+app
+    .use(router.routes())
+    .use(router.allowedMethods())
+;
 
 var port = process.env.PORT || 3000;
 app.listen(port);
+console.log('listening on port 3000');
