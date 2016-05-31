0f61281ebc623513b89ffa19172536ed985855ea
alexcong <alexcong@gmail.com>
2016-05-31 01:47:22 -0400

    using generator for mongoose

--- server/app.js
+++ server/app.js
@@ -4,6 +4,7 @@
 var app = require('koa')();
 var middlewares = require('koa-middlewares');
 var path = require('path');
+var mongoose = require('mongoose');
 
 app
     .use(middlewares.staticCache(path.resolve(__dirname, '../client/public')))
@@ -13,19 +14,33 @@ app
     .use(middlewares.logger())
 ;
 
-//temp var
-var counter = 0;
+/*database*/
+mongoose.connect('mongodb://localhost/test');
+var Schema = mongoose.Schema;
+var CounterSchema = new Schema({
+    counterId: Number,
+    likesCounter: Number
+});
+
+var Counter = mongoose.model('Counter', CounterSchema);
+var counter0 = new Counter({counterId: 0, likesCounter: 0});
+counter0.save((err)=> {
+    if (err) {
+        console.log(err);
+    }
+});
 
 /* router */
 var router = middlewares.router();
 router
     .get('/likescounter', function*(next) {
-        this.body = counter;
-        console.log('counter', counter);
+        let counter = yield Counter.findOne({counterId: 0});
+        this.response.body = counter.likesCounter;
     })
     .post('/likescounter', function*(next) {
-        counter = this.request.body.likesCounter;
-        console.log('counter', counter);
+        let counter = yield Counter.findOne({counterId: 0});
+        counter.likesCounter = this.request.body.likesCounter;
+        yield counter.save();
         this.status = 200;
     })
 ;
